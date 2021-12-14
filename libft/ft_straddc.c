f7bbc4dae104ea2fe9c6d9fb5c5f5f5f832d5bd5
user42 <eliarisalvares@gmail.com>
2021-12-14 14:35:46 -0300

    major updates

---
+++ libft/ft_straddc.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_straddc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 23:23:53 by elraira-          #+#    #+#             */
+/*   Updated: 2021/12/13 23:24:02 by elraira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_straddc(char *str, char c)
+{
+	char	*add;
+	int		i;
+
+	if (!c)
+		return (NULL);
+	if (!str)
+		return (ft_straddc_first(c));
+	add = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2));
+	if (!add)
+	{
+		free(str);
+		return (NULL);
+	}
+	i = -1;
+	while (str[++i])
+		add[i] = str[i];
+	free(str);
+	add[i++] = c;
+	add[i] = '\0';
+	return (add);
+}
---
+++ sources/server.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/09 14:02:22 by elraira-          #+#    #+#             */
+/*   Updated: 2021/12/14 17:03:33 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minitalk.h"
+
+void	ft_server_error_message_and_exit(int pid, char *str)
+{
+	if (str)
+		free(str);
+	ft_putstr("An unexpected error occurred in server execution\n");
+	kill(pid, SIGUSR2);
+	exit(EXIT_FAILURE);
+}
+
+char	*ft_print_message(char *message)
+{
+	ft_putstr(message);
+	ft_putchar('\n');
+	free(message);
+	return (NULL);
+}
+
+void	ft_server_user_signal_handler(int sig, siginfo_t *info, void *context)
+{
+	static char	c = 0b11111111;
+	static int	bits = 0;
+	static int	pid = 0;
+	static char	*message = 0;
+
+	(void)context;
+	if (info->si_pid)
+		pid = info->si_pid;
+	if (sig == SIGUSR1)
+		c ^= 0b10000000 >> bits;
+	else if (sig == SIGUSR2)
+		c |= 0b10000000 >> bits;
+	if (++bits == 8)
+	{
+		if (c != '\0')
+			message = ft_straddc(message, c);
+		else
+			message = ft_print_message(message);
+		bits = 0;
+		c = 0b11111111;
+	}
+	if (kill(pid, SIGUSR1) == -1)
+		ft_server_error_message_and_exit(pid, message);
+}
+
+/* 
+ * The sigset_t data type is used to represent a signal set. Internally, 
+ * it may be implemented as either an integer or structure type. 
+*/
+
+int	main(void)
+{
+	struct sigaction	sa_sig;
+	sigset_t			block_mask;
+
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask, SIGINT);
+	sigaddset(&block_mask, SIGQUIT);
+	sa_sig.sa_handler = 0;
+	sa_sig.sa_flags = SA_SIGINFO;
+	sa_sig.sa_mask = block_mask;
+	sa_sig.sa_sigaction = ft_server_user_signal_handler;
+	sigaction(SIGUSR1, &sa_sig, NULL);
+	sigaction(SIGUSR2, &sa_sig, NULL);
+	ft_putstr("Use this PID number to send your message: ");
+	ft_putnbr(getpid());
+	ft_putstr("\n");
+	while (1)
+		pause();
+}
